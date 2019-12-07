83d456d4f2fbb8a3050462a640097cc52faab6da
Israel Torres <israeltorres@uadec.edu.mx>
2019-12-06 22:13:48 -0600

    Pequeño cambio a la barra
    Ahora salen etiquetas al poner el mouse sobre los iconos

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,12 +1,12 @@
 <nav class="vertical-menu">
     <h1 style="color:aliceblue; font-size:large; text-align: center; margin-top: 1em;">{{ config('app.name', 'Laravel') }}</h1>
-    <a href="Dashboard" class="{{ Request::is('Dashboard') ? 'active' : '' }}"><span class="icon fa-home"></span></a>
-    <a href="productos" class="{{ Request::is('productos') ? 'active' : '' }}"><span class="fas fa-ice-cream"></span></a>
-    <a href="materiales" class="{{ Request::is('materiales') ? 'active' : '' }}"><span class="fas fa-candy-cane"></span></a>
-    <a href="Ordenes" class="{{ Request::is('Ordenes') ? 'active' : '' }}"><span class="fas fa-boxes"></span></a>
-    <a href="Categorias" class="{{ Request::is('Categorias') ? 'active' : '' }}"><span class="far fa-address-book"></span></a>
-    <a href="Unidadesmedida" class="{{ Request::is('Unidadesmedida') ? 'active' : '' }}"><span class="fas fa-ruler"></span></a>
-    <a href="{{ route('logout') }}" onclick="event.preventDefault(); 
+    <a title="Panel de control" href="Dashboard" class="{{ Request::is('Dashboard') ? 'active' : '' }}"><span class="icon fa-home"></span></a>
+    <a title="Productos" href="productos" class="{{ Request::is('productos') ? 'active' : '' }}"><span class="fas fa-ice-cream"></span></a>
+    <a title="Materiales" href="materiales" class="{{ Request::is('materiales') ? 'active' : '' }}"><span class="fas fa-candy-cane"></span></a>
+    <a title="Ordenes" href="Ordenes" class="{{ Request::is('Ordenes') ? 'active' : '' }}"><span class="fas fa-boxes"></span></a>
+    <a title="Categorias" href="Categorias" class="{{ Request::is('Categorias') ? 'active' : '' }}"><span class="far fa-address-book"></span></a>
+    <a title="Unidades de Medida" href="Unidadesmedida" class="{{ Request::is('Unidadesmedida') ? 'active' : '' }}"><span class="fas fa-ruler"></span></a>
+    <a title="Cerrar Sesion" href="{{ route('logout') }}" onclick="event.preventDefault(); 
         document.getElementById('logout-form').submit();">
         <i class="fas fa-sign-out-alt"></i>
     </a>
